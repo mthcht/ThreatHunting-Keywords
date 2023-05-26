52dca01f0e3dcb70ea90a1f5f3330c42cb3713f9
mthcht <mathieuchotplassot@gmail.com>
2023-05-26 22:43:20 +0200

    spliting the lookups

---
+++ split_threathunting-keywords.ps1
@@ -0,0 +1,19 @@
+﻿# Variables
+$csvFilePath = Join-Path $PSScriptRoot 'threathunting-keywords.csv'
+
+# Load CSV data
+$data = Import-Csv -Path $csvFilePath
+
+# Get unique metadata_keyword_type values
+$types = $data | Select-Object -ExpandProperty metadata_keyword_type -Unique
+
+foreach ($type in $types) {
+    # Filter data based on current type
+    $filteredData = $data | Where-Object {$_.metadata_keyword_type -eq $type}
+
+    # Define output file path
+    $outputFilePath = Join-Path $PSScriptRoot "$type.csv"
+
+    # Export filtered data to CSV
+    $filteredData | Export-Csv -Path $outputFilePath -NoTypeInformation
+}
