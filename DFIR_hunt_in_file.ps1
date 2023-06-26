30d99a5996285b0458ce9f8201522a55fd6a970b
mthcht <mathieuchotplassot@gmail.com>
2023-06-26 09:45:57 +0200

    Create DFIR_hunt_in_file.ps1

---
+++ DFIR_hunt_in_file.ps1
@@ -0,0 +1,24 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$patternFile,
+    [Parameter(Mandatory=$true)]
+    [string]$targetFile,
+    [Parameter(Mandatory=$true)]
+    [string]$rgPath
+)
+
+Start-Transcript -Path "$PSScriptRoot\result_search.log" -Append -Force -Verbose
+
+<#
+  Exemple usage: powershell -ep Bypass -File .\DFIR_hunt_in_file.ps1 -patternFile "only_keywords_regex.txt" -targetFile "C:\Users\mthcht\collection\20230406154410_EvtxECmd_Output.csv" -rgPath "C:\Users\mthcht\Downloads\ripgrep-13.0.0-x86_64-pc-windows-msvc\ripgrep-13.0.0-x86_64-pc-windows-msvc\rg.exe"
+#>
+
+$totalLines = (Get-Content $patternFile | Measure-Object -Line).Lines
+$currentLine = 0
+Get-Content $patternFile | ForEach-Object {
+    $currentLine++
+    Write-Host "Searching for pattern $currentLine of $totalLines : $_"  
+    & $rgPath --multiline --ignore-case $_ $targetFile | Write-Output 
+}
+
+Stop-Transcript -Verbose
